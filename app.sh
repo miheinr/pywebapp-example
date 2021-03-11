c363810a6c64eb4f79eb6986947f6c5107e171ce
Michael Heinrich <michael.heinrich@atos.net>
2021-03-11 15:39:41 +0100

    configure workers based on cores

--- app.sh
+++ app.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-echo "Starting Application"
-
 # export FLASK_APP=webapp.py
 # python3 -m flask run
 
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+# recommended amount of workers for gunicorn is (2*num_cores + 1)
+WORKERS=`python3 -c 'from multiprocessing import cpu_count; print(2*cpu_count() + 1)'`
+
+gunicorn --bind 0.0.0.0:8080 --workers $WORKERS wsgi:app
