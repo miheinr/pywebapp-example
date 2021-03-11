f4ccbddb9d1ad03fb0b185dfeabe8bfbc378d059
Michael Heinrich <michael.heinrich@atos.net>
2021-03-11 15:39:26 +0100

    configure cpuload in app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pywebapp-example
 An example for a Webapp in Python and Flasks.
+
+Each GET produces also cpuload for an amount of time which can be
+configured via environment variable STRESSTIME (in seconds).
+Default STRESSTIME is 0.
+
--- webapp.py
+++ webapp.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import cpuload_single
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    cpuload_single.cpuload()
     return 'Hello, World!'
 
 @app.route('/<name>')
 def hello_name(name):
+    cpuload_single.cpuload()
     return f'Hello, {name}!'
